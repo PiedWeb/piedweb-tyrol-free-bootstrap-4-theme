63f54f628553484bf9221d0f9195866c18309167
Robin <contact@robin-d.fr>
2020-06-04 10:10:45 +0200

    Minor update

--- src/js/app.js
+++ src/js/app.js
@@ -7,12 +7,12 @@
  * Import CSS
  */
 
-import BootstrapCookieConsent from 'bootstrap-cookie-consent'
+import BootstrapCookieConsent from 'bootstrap-cookie-consent';
 
-import baguetteBox from 'baguettebox.js'
+import baguetteBox from 'baguettebox.js';
 
-import Macy from 'macy'
-import '@babel/polyfill'
+import Macy from 'macy';
+import '@babel/polyfill';
 
 import {
   fixedNavBar,
@@ -23,63 +23,63 @@ import {
   smoothScroll,
   applySmoothScroll,
   addAClassOnScroll,
-  allClickable
-} from './helpers.js'
+  allClickable,
+} from './helpers.js';
 
-require('../scss/main.scss')
+require('../scss/main.scss');
 
-var bsn = require('bootstrap.native/dist/bootstrap-native-v4')
+var bsn = require('bootstrap.native/dist/bootstrap-native-v4');
 
 document.addEventListener('DOMContentLoaded', function () {
-  applyOnDomLoaded()
+  applyOnDomLoaded();
   /**/
   new BootstrapCookieConsent({
     services: ['StatistiquesAnonymes', 'YouTube'],
     services_descr: {
       StatistiquesAnonymes:
         "Nous permet d'améliorer le site en fonction de son utilisation",
-      YouTube: 'Affiche les vidéos du service youtube.com'
+      YouTube: 'Affiche les vidéos du service youtube.com',
     },
-    method: 'bsn'
-  })
+    method: 'bsn',
+  });
   /**/
 
-  addAClassOnScroll('.navbar', 'nostick', 50)
-})
+  addAClassOnScroll('.navbar', 'nostick', 50);
+});
 
 var gallery = function () {
   baguetteBox.run('.mimg', {
     captions: function (element) {
-      return element.getElementsByTagName('img')[0].alt
-    }
-  })
-}
+      return element.getElementsByTagName('img')[0].alt;
+    },
+  });
+};
 
-async function applyOnDomLoaded () {
-  await backgroundLazyLoad()
-  fixedNavBar()
-  allClickable('.clickable')
-  applySmoothScroll()
-  gallery()
+async function applyOnDomLoaded() {
+  await backgroundLazyLoad();
+  fixedNavBar();
+  allClickable('.clickable');
+  applySmoothScroll();
+  gallery();
   var masonry = new Macy({
     container: '#flex-masonry',
     columns: 3,
     margin: {
       y: 16,
-      x: '2%'
+      x: '2%',
     },
     breakAt: {
       992: 2,
-      768: 1
-    }
-  })
+      768: 1,
+    },
+  });
 }
 
-function applyOnDomChanged () {
-  gallery()
+function applyOnDomChanged() {
+  gallery();
 }
 
-document.addEventListener('linksBuilt', applyOnDomChanged)
+document.addEventListener('linksBuilt', applyOnDomChanged);
 
 // var observer = new MutationObserver(applyOnDomChanged)
 // observer.observe(document.body, { attributes: true, childList: true, subtree: true })
--- webpack.config.js
+++ webpack.config.js
@@ -1,94 +1,93 @@
-require('webpack')
+require('webpack');
 
-const webpack = require('webpack')
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const WebpackPwaManifest = require('webpack-pwa-manifest')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const config = {
   mode: 'development',
-  entry: [
-    './src/js/app.js'
-  ],
+  entry: ['./src/js/app.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   module: {
-    rules: [{
-      test: /\.s?[ac]ss$/,
-      use: [
-        /**
+    rules: [
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          /**
                     {
                         loader: 'style-loader', // translates CSS into CommonJS modules
                         options: {
                             sourceMap: true
                         }
                     },**/
-        MiniCssExtractPlugin.loader,
-        'css-loader',
-        {
-          loader: 'postcss-loader', // Run post css actions
-          options: {
-            sourceMap: true,
-            plugins: function () {
-              return [
-                // require('postcss-flexbugs-fixes'),
-                // require('autoprefixer')
-              ]
-            }
-          }
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader', // Run post css actions
+            options: {
+              sourceMap: true,
+              plugins: function () {
+                return [
+                  // require('postcss-flexbugs-fixes'),
+                  // require('autoprefixer')
+                ];
+              },
+            },
+          },
+          {
+            loader: 'sass-loader', // compiles SASS to CSS
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|ttf|woff2|woff|eot)$/,
+        use: 'file-loader',
+      },
+      {
+        test: /.html$/,
+        use: {
+          loader: 'html-loader',
         },
-        {
-          loader: 'sass-loader', // compiles SASS to CSS
-          options: {
-            sourceMap: true
-          }
-        }
-      ]
-    },
-    {
-      test: /\.(png|jpg|gif|svg|ttf|woff2|woff|eot)$/,
-      use: 'file-loader'
-    },
-    {
-      test: /.html$/,
-      use: {
-        loader: 'html-loader'
-      }
-    },
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: [{
-        loader: 'babel-loader', // transpile to ES5
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      }]
-    }
-    ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader', // transpile to ES5
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     // new CopyWebpackPlugin([{from:'./src/demo/html/img', to: 'img'}]),
     new MiniCssExtractPlugin({
-      filename: 'app.css'
+      filename: 'app.css',
     }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/demo/html/index.html'
+      template: './src/demo/html/index.html',
     }),
     new HtmlWebpackPlugin({
       filename: 'elements.html',
-      template: './src/demo/html/elements.html'
-    })
-  ]
-}
+      template: './src/demo/html/elements.html',
+    }),
+  ],
+};
 
 var devConfig = Object.assign({}, config, {
   mode: 'development',
@@ -96,10 +95,8 @@ var devConfig = Object.assign({}, config, {
     contentBase: path.join(__dirname, 'dist'),
     open: true,
     openPage: 'index.html',
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+});
 
-module.exports = [
-  devConfig
-]
+module.exports = [devConfig];
